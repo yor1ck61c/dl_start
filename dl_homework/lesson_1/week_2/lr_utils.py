4e94430a663ed0e7cf76c5e784004d7d74dbb5bb
yorick_61c <1240371570@qq.com>
2022-01-17 17:48:34 +0800

    完成了猫猫分类器，实现对猫猫图片的分辨，支持测试自己的图片。

--- dl_homework/lesson_1/week_2/lr_utils.py
+++ dl_homework/lesson_1/week_2/lr_utils.py
@@ -2,10 +2,10 @@ import numpy as np
 import h5py
  
 def load_dataset():
-    train_dataset = h5py.File('dl_homework/lesson_1/week_2/datasets/train_catvnoncat.h5', "r")
+    train_dataset = h5py.File('E:/VSCodeWorkSpace/dl_start/dl_homework/lesson_1/week_2/datasets/train_catvnoncat.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
-    test_dataset = h5py.File('dl_homework/lesson_1/week_2/datasets/test_catvnoncat.h5', "r")
+    test_dataset = h5py.File('E:/VSCodeWorkSpace/dl_start/dl_homework/lesson_1/week_2/datasets/test_catvnoncat.h5', "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
     classes = np.array(test_dataset["list_classes"][:]) # the list of classes
